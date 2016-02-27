c98e0ed2e010b84699e146c8a70e9c24a969c2c4
Shing Lyu <shing.lyu@gmail.com>
2016-02-28 05:52:58 +0800

    init commit

---
+++ install.sh
@@ -0,0 +1,8 @@
+# virtualenv venv
+source venv/bin/activate
+
+pip install jupyter #ipython notebook
+sudo apt-get install libatlas-base-dev gfortran # for scipy
+pip install scipy
+pip install scikit-learn
+pip install matplotlib
